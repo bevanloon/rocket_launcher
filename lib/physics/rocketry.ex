7c70e47a680b709035f2f9a6171e2291cf0965bb
Bevan <sendbevanemail@gmail.com>
2015-12-31 21:52:53 +0000

    refactor to allow any planet into our orbital calcs

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -19,40 +19,42 @@ defmodule Physics.Rocketry do
       |> nearest_tenth
   end
 
-  def orbital_speed(height) do
-    newtons_gravitational_constant * earth.mass  / orbital_radius(height)
+  def orbital_speed(height,  %{mass: mass, radius: radius}) do
+    newtons_gravitational_constant * mass  /
+      orbital_radius(height, radius)
     |> square_root
   end
 
-  def orbital_acceleration(height) do
-    (orbital_speed(height) |> squared) / orbital_radius(height)
+  def orbital_acceleration(height, %{mass: mass, radius: radius}) do
+    (orbital_speed(height, %{mass: mass, radius: radius}) |> squared) /
+      orbital_radius(height, radius)
   end
 
-  def orbital_term(height) do
+  def orbital_term(height, %{mass: mass, radius: radius}) do
     4 * (:math.pi |> squared) *
-        (orbital_radius(height) |> cubed) /
+        (orbital_radius(height, radius) |> cubed) /
         (newtons_gravitational_constant
-          * earth.mass)
+          * mass)
       |> square_root
       |> seconds_to_hours
   end
 
-  def height_for_orbital_term(term) do
+  def height_for_orbital_term(term, %{mass: mass, radius: radius}) do
       (newtons_gravitational_constant *
-        earth.mass *
+        mass *
         (term |> seconds_to_hours |> squared)  /
         (4 * (:math.pi |> squared)))
         |> cube_root
-        |> height_sans_orbital_radius
+        |> height_sans_orbital_radius(radius)
         |> to_km
   end
 
-  defp height_sans_orbital_radius(height) do
-    earth.radius - height
+  defp height_sans_orbital_radius(height, radius) do
+    radius - height
   end
 
-  defp orbital_radius(height) do
-    earth.radius + (height |> to_m)
+  defp orbital_radius(height, radius) do
+    radius + (height |> to_m)
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
--- test/rocketry_test.exs
+++ test/rocketry_test.exs
@@ -16,16 +16,16 @@ defmodule RocketryTests do
   end
 
   test "orbital term greater than 3 hours" do
-    assert orbital_term(6366) > 3
+    assert orbital_term(6366, earth) > 3
   end
 
   test "orbital term less than 5 hours" do
-    assert orbital_term(6366) < 5
+    assert orbital_term(6366, earth) < 5
   end
 
   # chapter 6 extra credit
   test "height for orbital term 4 hours" do
-    r = height_for_orbital_term(4)
+    r = height_for_orbital_term(4, earth)
     #only need an approximate to complete chapter 6 extra credit
     assert (trunc(r)) == 6365
   end
